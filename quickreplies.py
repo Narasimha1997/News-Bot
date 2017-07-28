b03c910b31070b0e37754017bd6e16a4c76fc78f
Narasimha1997 <yogaimpression@gmail.com>
2017-07-28 11:26:36 +0530

    news

--- quickreplies.py
+++ quickreplies.py
@@ -1,14 +1,11 @@
 from fbmq import QuickReply
 
-quickreplies=['bloomberg','cnn','business-insider','bbc-news','cnbc','hacker-news','techcrunch','talksport','google-news','national-geographic','the-next-web',
-'the-verge','ign','the-hindu','the-times-of-india']
+quickreplies=['bloomberg','cnn','business-insider','bbc-news','cnbc','hacker-news','techcrunch','talksport','google-news','national-geographic','the-times-of-india']
 
 def get_news_quick_reply():
     return [QuickReply(title='bloomberg',payload='bloomberg'),QuickReply(title='cnn',payload='cnn'),
     QuickReply(title='business-insider',payload='business-insider'),QuickReply(title='bbc-news',payload='bbc-news'),
     QuickReply(title='cnbc',payload='cnbc'),QuickReply(title='hacker-news',payload='hacker-news'),
     QuickReply(title='techcrunch',payload='techcrunch'),QuickReply(title='talksport',payload='talksport'),
-    QuickReply(title='google-news',payload='google-news'),QuickReply(title='national-geographic',payload='national-geographic'),
-    QuickReply(title='the-next-web',payload='the-next-web'),QuickReply(title='the-verge',payload='the-verge'),
-    QuickReply(title='ign',payload='ign'),QuickReply(title='the-hindu',payload='the-hindu'),
+    QuickReply(title='google-news',payload='google-news'),
     QuickReply(title='the-times-of-india',payload='the-times-of-india')]
