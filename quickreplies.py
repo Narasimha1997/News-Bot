52f60ce4e8a109352d243ff72e9dcb65b8fb80a9
Narasimha1997 <yogaimpression@gmail.com>
2017-07-28 10:30:41 +0530

    news

--- quickreplies.py
+++ quickreplies.py
@@ -5,4 +5,4 @@ quickreplies=['bloomberg','cnn','business-insider','bbc-news','cnbc','hacker-new
 def get_news_quick_reply():
     return [QuickReply(title='bloomberg',payload='bloomberg'),QuickReply(title='cnn',payload='cnn'),
     QuickReply(title='business-insider',payload='business-insider'),QuickReply(title='bbc-news',payload='bbc-news'),
-    QuickReply(title='cnbc',payload='cnbc'),QuickReply(title='hacker-news'.payload='hacker-news')]
+    QuickReply(title='cnbc',payload='cnbc'),QuickReply(title='hacker-news',payload='hacker-news')]
